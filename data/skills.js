b95e3d0b25feae728b2ecfb81d5ea95bbc90f397
l4tte <pee35225@gmail.com>
2020-09-22 01:39:07 +0700

    ..

--- data/skills.js
+++ data/skills.js
@@ -1,5 +1,5 @@
 export const skills = {
-  technologies: ['HTML5', 'CSS', 'JavaScript', 'Java'],
+  technologies: ['HTML5', 'CSS', 'JavaScript', 'Java(basic)'],
   frameworks: ['React', 'NextJs'],
   databases: ['MySQL', 'MongoDB', 'Firebase'],
   others: ['Vscode', 'Xcode', 'Git'],
